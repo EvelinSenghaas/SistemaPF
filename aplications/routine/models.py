5f4dd888c83be44f4dc0c8f62471e7e06d778979
Nicolas <nico.290698@gmail.com>
2019-08-26 20:04:21 -0300

    Rutina Creacion de clase rutina

--- aplications/routine/models.py
+++ aplications/routine/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Rutina(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 50, blank = False, null = False)
+    descripcion = models.TextField(blank = False, null = False)
+
+
--- sistema/settings.py
+++ sistema/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aplications.routine',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'sistema.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 TIME_ZONE = 'UTC'
 
